3c583de374d1f79774bc37e53f8c207aca3bc9da
kkanishkan <kanishkan77@gmail.com>
2018-12-16 20:45:47 -0500

    Last

--- A1-Prototype1/A1-Prototype1/itemsList.Designer.vb
+++ A1-Prototype1/A1-Prototype1/itemsList.Designer.vb
@@ -26,7 +26,6 @@ Partial Class itemsList
         Me.Label1 = New System.Windows.Forms.Label()
         Me.ListBox1 = New System.Windows.Forms.ListBox()
         Me.Label2 = New System.Windows.Forms.Label()
-        Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
@@ -74,15 +73,6 @@ Partial Class itemsList
         Me.Label2.TabIndex = 7
         Me.Label2.Text = "Favourite Stores"
         '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(0, 0)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 8
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
-        '
         'Button2
         '
         Me.Button2.BackColor = System.Drawing.Color.DarkRed
@@ -100,7 +90,6 @@ Partial Class itemsList
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.ListBox1)
         Me.Controls.Add(Me.InventoryItemPanel)
@@ -116,6 +105,5 @@ Partial Class itemsList
     Friend WithEvents Label1 As Label
     Friend WithEvents ListBox1 As ListBox
     Friend WithEvents Label2 As Label
-    Friend WithEvents Button1 As Button
     Friend WithEvents Button2 As Button
 End Class
